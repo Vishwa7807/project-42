fc7f23a18ec4b775714ff6f8ffa1725a30a6093b
Vishwa7807 <81681334+Vishwa7807@users.noreply.github.com>
2021-05-10 15:09:23 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,28 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+        //load Image for BestMan
+        this.image1 = loadImage("Bestman-01.png");
+        
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        
+        //display the image for BestMan if the frameCount is over 200, otherwise display the boy with umbrella image
+        if(frameCount >= 200){
+            imageMode(CENTER);
+            image(this.image1,pos.x,pos.y+70,300,300);
+        }
+        else {
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300)
+        }
+    }
+}
---
+++ drops.js
@@ -0,0 +1,24 @@
+class drops{
+    constructor(x,y){
+       var option={
+            restitution: 0.1,
+            friction: 0.01
+
+        }
+        this.body = Bodies.circle(x,y,5,option);
+        this.radius = 5;
+        World.add(world,this.body);
+    }
+    update(){
+        if(this.body.position.y>height){
+            Matter.Body.setPosition(this.body,{x: random(0,400),y: random(0,400)});
+        }
+    }
+    display(){
+        noStroke();
+        fill(0,0,255);
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+
+    }
+}
